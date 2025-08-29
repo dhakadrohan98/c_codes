9e545f6161cbe85fa6827a1c941d22ee1e008a57
RohanDhakad <dhakadrohan98>
2025-08-30 00:11:10 +0530

    scanf is used

--- crashcourse/main.c
+++ crashcourse/main.c
@@ -8,6 +8,10 @@ int main()
     int** pptr = &a;
     printf("p is %d, a's address is %u\n", p, a);
     printf("a's address is %u\n", a);
-    printf("pptr's value is %u", **pptr);
+    printf("pptr's value is %u\n", **pptr);
+    float x;
+    printf("Enter amount you want to withdraw");
+    scanf("%f", &x);
+    printf("x value is %f", x);
     return 0;
 }
